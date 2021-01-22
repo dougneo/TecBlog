203ea939507a568484955f2a9ee24ac8a411ecb5
Douglas <doug.neo@gmail.com>
2021-01-21 22:58:46 -0300

    Adicionado novas funcionalidades

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 function Mudarestado(el) {
     var display = document.getElementById(el).style.display;
-    if(display == "none")
-        document.getElementById(el).style.display = 'block';
-    else
+    if (display == "block")
         document.getElementById(el).style.display = 'none';
+    else
+        document.getElementById(el).style.display = 'block';
+
 }
